6818879ec2d5bda244604ce12833253bf6124c7a
MexicoLayonzz <fercho09mar@gmail.com>
2025-05-01 22:41:29 -0600

    Actualización de librerias y testeo de la creación de los objetos Materio y Pregunta

--- src/App.java
+++ src/App.java
@@ -1,8 +1,18 @@
 
+import backend.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class App 
 {
     public static void main(String[] args) throws Exception 
     {
-        System.out.println("Hello, World!");
+        Materia materia = new Materia("Matematicas", "Algebra");
+        materia.addPregunta(new Pregunta("200", "Punto cruz", "¿A*B = B*A teniendo que A y B son dos matrices de tamaño n?", "No son iguales"));
+        materia.addPregunta(new Pregunta("100", "Determinante", "¿Como se saca el determinante?", "Por la regla de Sarrus"));
+
+        JSONArray jsonArray = new JSONArray(materia.getPreguntas());
     }
 }
